95219e07c3f5af1f7c2cd5f03ddd0018e8c0f120
Brandon Stalnaker <bstalnaker@mparticle.com>
2023-03-02 14:11:22 -0500

    try 6

--- Package.swift
+++ Package.swift
@@ -22,8 +22,7 @@ let package = Package(
         .target(
             name: "mParticle-CleverTap",
             dependencies: ["mParticle-Apple-SDK", "CleverTapSDK"],
-            path: "mParticle-CleverTap",
-            exclude: ["Info.plist", "mParticle_CleverTap.h"]
+            path: "mParticle-CleverTap"
         )
     ]
 )
