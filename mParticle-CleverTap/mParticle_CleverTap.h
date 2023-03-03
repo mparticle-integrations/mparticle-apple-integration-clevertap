2771c5c1ad17e17832e87b81a15264322786e549
Brandon Stalnaker <bstalnaker@mparticle.com>
2023-03-03 10:24:14 -0500

    ignore header in swift

--- mParticle-CleverTap/mParticle_CleverTap.h
+++ mParticle-CleverTap/mParticle_CleverTap.h
@@ -2,5 +2,7 @@
 
 FOUNDATION_EXPORT double mParticle_ExampleVersionNumber;
 FOUNDATION_EXPORT const unsigned char mParticle_ExampleVersionString[];
-
+#if SWIFT_PACKAGE
+#else
 #import <mParticle_CleverTap/MPKitCleverTap.h>
+#endif
