35e036c6867d8c6ba666c36102bae3151b7a99c9
Brandon Stalnaker <bstalnaker@mparticle.com>
2023-03-03 11:05:41 -0500

    fix misstype

--- mParticle-CleverTap/mParticle_CleverTap.h
+++ mParticle-CleverTap/mParticle_CleverTap.h
@@ -2,6 +2,8 @@
 
 FOUNDATION_EXPORT double mParticle_ExampleVersionNumber;
 FOUNDATION_EXPORT const unsigned char mParticle_ExampleVersionString[];
-#if defined(__has_include) && __has_include(<mParticle_CleverTap/MPKitCleverTap.h>>)
+#if defined(__has_include) && __has_include(<mParticle_CleverTap/MPKitCleverTap.h>)
 #import <mParticle_CleverTap/MPKitCleverTap.h>
+#else
+#import "MPKitCleverTap.h"
 #endif
