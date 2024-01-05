108cb09fc7c7085a94f50fb76072357e9d9b64ea
Ben Baron <bbaron@mparticle.com>
2024-01-05 16:15:48 -0500

    feat: Set custom SDK name and version on CleverTap

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -5,6 +5,9 @@ NOTES="$2"
 # Update version number
 #
 
+# Update constant in codebase
+sed -i '' 's/NSString \*const kLibVersion = @".*/NSString *const kLibVersion = @"'"$VERSION"'";/' mParticle-CleverTap/MPKitCleverTap.m
+
 # Update CocoaPods podspec file
 sed -i '' 's/\(^    s.version[^=]*= \).*/\1"'"$VERSION"'"/' mParticle-CleverTap.podspec
 
@@ -12,8 +15,7 @@ sed -i '' 's/\(^    s.version[^=]*= \).*/\1"'"$VERSION"'"/' mParticle-CleverTap.
 #
 
 git add mParticle-CleverTap.podspec
-git add mParticle_CleverTap.json
-git add CHANGELOG.md
+git add mParticle-CleverTap/MPKitCleverTap.m
 git commit -m "chore(release): $VERSION [skip ci]
 
 $NOTES"
